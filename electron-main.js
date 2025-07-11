3874d39a2b483696bbbac2ac2ff23d66c2a5fb48
KNODRON <juan.jimenezv08@gmail.com>
2025-07-11 11:36:04 -0400

    Update electron-main.js

--- electron-main.js
+++ electron-main.js
@@ -1,1 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const serve = require('electron-serve')
+
+const loadURL = serve({ directory: path.join(__dirname, 'dist') })
+
+async function createWindow() {
+  await loadURL()
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: { contextIsolation: true }
+  })
+  win.removeMenu()
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => app.quit())
 
