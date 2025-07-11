61d18f066bdba8bba869a860839de6fe076a6b8d
KNODRON <juan.jimenezv08@gmail.com>
2025-07-11 12:46:02 -0400

    Update electron-main.js

--- electron-main.js
+++ electron-main.js
@@ -1,19 +1,27 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const serve = require('electron-serve')
-
-const loadURL = serve({ directory: path.join(__dirname, 'dist') })
 
 async function createWindow() {
-  await loadURL()
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
-    webPreferences: { contextIsolation: true }
+    webPreferences: {
+      contextIsolation: true
+    }
   })
+
+  if (process.env.NODE_ENV === 'development') {
+    // En dev, carga la PWA de Vite en localhost
+    await win.loadURL('http://localhost:5173')
+  } else {
+    // En producción, carga el build estático
+    await win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  }
+
   win.removeMenu()
 }
 
 app.whenReady().then(createWindow)
 app.on('window-all-closed', () => app.quit())
+
 
